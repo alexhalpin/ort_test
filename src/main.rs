0c77a8a6695f6fd0eacdd8bc6608c51da63552ea
alex <alexhalpin0@gmail.com>
2024-05-03 23:44:34 -0400

    change input

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let model = ort::Session::builder()?
         .commit_from_file(Path::new(env!("CARGO_MANIFEST_DIR")).join("rf_iris.onnx"))?; // replace with your own model
 
-    let model_input: Vec<f32> = vec![6.3, 3.3, 6., 2.5];
+    let model_input: Vec<f32> = vec![4.4, 3.2, 1.3, 0.2];
     let outputs = model.run(inputs!["X" => ([1usize, 4usize], model_input) ]?)?;
 
     let output = outputs["output_label"]
