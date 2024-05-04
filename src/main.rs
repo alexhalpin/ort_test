eda8393cc9bd0f9f9a64609a953cf41b0195a9b4
alex <alexhalpin0@gmail.com>
2024-05-04 10:55:45 -0400

    2 test examples

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let model = ort::Session::builder()?
         .commit_from_file(Path::new(env!("CARGO_MANIFEST_DIR")).join("rf_iris.onnx"))?; // replace with your own model
 
-    let model_input: Vec<f32> = vec![4.4, 3.2, 1.3, 0.2];
-    let outputs = model.run(inputs!["X" => ([1usize, 4usize], model_input) ]?)?;
+    let model_input: Vec<f32> = vec![4.4, 3.2, 1.3, 0.2, 6.7, 2.5, 5.8, 1.8];
+    let outputs = model.run(inputs!["X" => ([2usize, 4usize], model_input) ]?)?;
 
-    let output = outputs["output_label"]
-        .try_extract_tensor::<i64>()?
-        .t()
-        .into_owned();
+    let output = outputs["output_label"].try_extract_tensor::<i64>()?;
 
     print!("output: {}", &output);
     Ok(())
